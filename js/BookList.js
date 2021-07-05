4d277b22dfca4baea2b6849a86a8ae41b37b9135
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-07-05 14:16:57 +0200

    Add clear and addBook functions

--- js/BookList.js
+++ js/BookList.js
@@ -1,29 +1,50 @@
-const bookUl = document.querySelector(".book-list");
+const bookUl = document.querySelector('.book-list');
 const bookList = [
-    {
-    title: "title",
-    author: "author",
+  {
+    title: 'title',
+    author: 'author',
     id: 0,
-    }
+  },
 ];
 
-function showbook (id){
-    bookObjective = bookList[id];
-    bookContainer = document.createElement('il');
-    bookTitle = document.createElement('h2');
-    bookAuthor = document.createElement('h2');
-    removeButton =document.createElement('button');
-    bar = document.createElement('div');
-    bar.classList.add('bar');
-    
-    bookTitle.textContent = bookObjective.title;
-    bookAuthor.textContent = bookObjective.author,
+function showbook (id) {
+  const bookObjective = bookList[id];
+  const bookContainer = document.createElement('il');
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('h2');
+  const removeButton = document.createElement('button');
+  const bar = document.createElement('div');
+  bar.classList.add('bar');
 
+  bookTitle.textContent = bookObjective.title;
 
-    bookContainer.appendChild(bookTitle);
-    bookContainer.appendChild(bookAuthor);
-    bookContainer.appendChild(removeButton)
-    bookContainer.appendChild(bar);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(removeButton);
+  bookContainer.appendChild(bar);
 
-    bookUl.appendChild(bookContainer);
+  bookUl.appendChild(bookContainer);
+  bookList.forEach((book) => {
+    let allBooks = book[0];
+    return allBooks;
+  });
 }
+
+const addBook = () => {
+  const book = {
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  };
+  bookList.push(book);
+};
+
+const clear = () => {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+};
+
+const save = document.getElementById('submit');
+save.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBook();
+  clear();
+});
